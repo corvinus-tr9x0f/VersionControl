f5afb2ecea74fffb42a71c6dba7a1a17299e7622
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-10-07 00:12:15 +0200

    Adatesxport - folyamatban

--- UserMaintenance/ExcelExportWeek04/Form1.cs
+++ UserMaintenance/ExcelExportWeek04/Form1.cs
@@ -69,5 +69,41 @@ namespace ExcelExportWeek04
                 xlApp = null;
             }
         }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                //values[counter, 4] = f.Lift;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = f.Price/f.FloorArea;
+                counter++;
+            }
+        }
     }
 }
