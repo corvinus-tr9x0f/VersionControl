d372f561bb28851c1449d9d793f26278a9c47437
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-10-07 15:36:03 +0200

    Kimenet formázása

--- UserMaintenance/ExcelExportWeek04/Form1.cs
+++ UserMaintenance/ExcelExportWeek04/Form1.cs
@@ -125,6 +125,26 @@ namespace ExcelExportWeek04
             xlSheet.get_Range(
              GetCell(2, 1),
              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range firstColoumn = xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), 1));
+            firstColoumn.Font.Bold = true;
+            firstColoumn.Interior.Color = Color.LightYellow;
+
+            Excel.Range lastColoumn = xlSheet.get_Range(GetCell(2, values.GetLength(1)), GetCell(1 + values.GetLength(0), values.GetLength(1)));
+            lastColoumn.Interior.Color = Color.LightGreen;
+            lastColoumn.NumberFormat = "###,###.00";
         }
     }
 }
