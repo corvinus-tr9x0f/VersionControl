8146d6d7939a24ef53946eb2db4561f63a52e6cc
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-10-20 11:56:49 +0200

    Portfólió tárolása a memóriában

--- UserMaintenance/VaRWeek05/Form1.cs
+++ UserMaintenance/VaRWeek05/Form1.cs
@@ -7,20 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaRWeek05.Entities;
 
 namespace VaRWeek05
 {
     public partial class Form1 : Form
     {
-        PortfolioEntities context = new PortfolioEntities();
-     
+        PortfolioEntities context = new PortfolioEntities();     
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        
         public Form1()
         {
             InitializeComponent();
 
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
