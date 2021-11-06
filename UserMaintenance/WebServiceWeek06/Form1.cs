6e63bfdb31d80dbc111f23787f1bbfbfab163d44
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-11-06 16:51:14 +0100

    Dinamikus funkciók

--- UserMaintenance/WebServiceWeek06/Form1.cs
+++ UserMaintenance/WebServiceWeek06/Form1.cs
@@ -22,26 +22,17 @@ namespace WebServiceWeek06
         {
             InitializeComponent();
 
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+
             GetWebService();
-            ProcessXML();
-
             dataGridView1.DataSource = Rates;
-
-            chartRateData.DataSource = Rates;
-
-            var series = chartRateData.Series[0];
-            series.ChartType = SeriesChartType.Line;
-            series.XValueMember = "Date";
-            series.YValueMembers = "Value";
-            series.BorderWidth = 2;
-
-            var legend = chartRateData.Legends[0];
-            legend.Enabled = false;
-
-            var chartArea = chartRateData.ChartAreas[0];
-            chartArea.AxisX.MajorGrid.Enabled = false;
-            chartArea.AxisY.MajorGrid.Enabled = false;
-            chartArea.AxisY.IsStartedFromZero = false;
+            ProcessXML();
+            CreateDiagram();
         }
 
         private void GetWebService()
@@ -50,9 +41,9 @@ namespace WebServiceWeek06
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -80,6 +71,40 @@ namespace WebServiceWeek06
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        private void CreateDiagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
