d08bbbc9825693038311fad08a4d23b5df5edb1c
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-11-12 20:29:02 +0100

    Absztrakció felhasználása a futószalagban (Form1 módosítások)

---
+++ UserMaintenance/FactoryPatternWeek08/Entities/Toys.cs
@@ -0,0 +1,20 @@
+﻿using FactoryPatternWeek08.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactoryPatternWeek08.Entities
+{
+    public class Toys : Toy
+    {       
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+    }
+}
--- UserMaintenance/FactoryPatternWeek08/Form1.cs
+++ UserMaintenance/FactoryPatternWeek08/Form1.cs
@@ -1,4 +1,5 @@
-﻿using FactoryPatternWeek08.Entities;
+﻿using FactoryPatternWeek08.Abstractions;
+using FactoryPatternWeek08.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,11 +14,11 @@ namespace FactoryPatternWeek08
 {
     public partial class Form1 : Form
     {
-        List<Ball> _balls = new List<Ball>();
+        List<Toy> _toys = new List<Toy>();
         
-        private BallFactory _factory;
+        private IToyFactory _factory;
 
-        public BallFactory Factory
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -32,26 +33,26 @@ namespace FactoryPatternWeek08
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var toy in _toys)
             {
-                ball.MoveToy();
-                if (ball.Left > maxPosition) maxPosition = ball.Left;
+                toy.MoveToy();
+                if (toy.Left > maxPosition) maxPosition = toy.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
-                mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                var oldestToy = _toys[0];
+                mainPanel.Controls.Remove(oldestToy);
+                _toys.Remove(oldestToy);
             }
         }
     }
