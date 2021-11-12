64ad60e876a2833a6023783a8edab1cb5e0edc07
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-11-12 16:53:16 +0100

    Gyár absztrakció kezelése

---
+++ UserMaintenance/FactoryPatternWeek08/Abstractions/IToy Factory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternWeek08.Abstractions
+{
+    public interface IToy_Factory
+    {
+        Toy CreateNew();
+    }
+}
--- UserMaintenance/FactoryPatternWeek08/Form1.cs
+++ UserMaintenance/FactoryPatternWeek08/Form1.cs
@@ -43,7 +43,7 @@ namespace FactoryPatternWeek08
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition) maxPosition = ball.Left;
             }
 
