50a454488344e7ff3f6030f641e5df6f6978a43c
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-11-25 13:19:29 +0100

    Jelszó validálás teszt

--- UserMaintenance/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UserMaintenance/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -25,5 +25,22 @@ namespace UnitTestExample.Test
 
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+        Test,
+        TestCase("abcdABCD", false),
+        TestCase("ABCD1234", false),
+        TestCase("abcd1234", false),
+        TestCase("Ab1234", false),
+        TestCase("Abcd1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
