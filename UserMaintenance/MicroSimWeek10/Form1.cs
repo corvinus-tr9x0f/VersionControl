744c9a2667fdcfeb64c772240e1b353c15df15cc
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-12-03 10:28:51 +0100

    Szimuláció

--- UserMaintenance/MicroSimWeek10/Form1.cs
+++ UserMaintenance/MicroSimWeek10/Form1.cs
@@ -18,6 +18,8 @@ namespace MicroSimWeek10
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
+        Random rng = new Random(1234);
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +27,23 @@ namespace MicroSimWeek10
             Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {                
+                for (int i = 0; i < Population.Count; i++)
+                {
+                  
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
 
@@ -90,6 +109,5 @@ namespace MicroSimWeek10
 
             return deathprobabilities;
         }
-
     }
 }
