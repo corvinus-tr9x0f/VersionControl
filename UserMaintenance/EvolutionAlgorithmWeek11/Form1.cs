e674be53e4873709bb02d4a2d3cec92cc1f05496
Judit Kiss <judit.kiss2@stud.uni-corvinus.hu>
2021-12-10 22:53:08 +0100

    A játék kipróbálása

--- UserMaintenance/EvolutionAlgorithmWeek11/Form1.cs
+++ UserMaintenance/EvolutionAlgorithmWeek11/Form1.cs
@@ -22,6 +22,9 @@ namespace EvolutionAlgorithmWeek11
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
